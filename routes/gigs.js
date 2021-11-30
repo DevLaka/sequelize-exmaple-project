9dd9f36f73e4fbf81d87895add4277e20555aea6
lakshan <lakshanwarunab@gmail.com>
2021-11-30 16:12:08 +0530

    add gigs findAll method

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,7 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require("../config/db");
+const Gig = require("../models/Gig");
 
-router.get('/', (req, res) => res.end('Gigs'));
+router.get("/", (req, res) =>
+  Gig.findAll()
+    .then((gigs) => {
+      console.log(gigs);
+      res.sendStatus(200);
+    })
+    .catch((err) => console.log(err))
+);
 
 module.exports = router;
-
